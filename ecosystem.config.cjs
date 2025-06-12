fc1e2ec2490dbc335dc8dda88885223af0ea0dd3
SenDoan <mariathanhsen@gmail.com>
2025-06-12 14:23:52 -0500

    update ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "SenDDT MW",
-      script: "yarn serve dist --single",
+      script: "yarn serve",
       env: {
         PORT: 3008,
         HOST: "127.0.0.1",

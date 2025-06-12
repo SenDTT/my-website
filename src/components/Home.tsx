e605e5f600449bee9f0826ac4955b463fcf7eaf2
SenDoan <mariathanhsen@gmail.com>
2025-06-12 15:54:49 -0500

    update SEO

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,8 +12,8 @@ export default function Home() {
                 description="Explore the portfolio of Sen Doan, a skilled web developer and designer. View projects, skills, and contact information to collaborate on innovative web solutions."
                 keywords="Sen Doan, portfolio, web developer, web designer, React developer, front-end developer, UI/UX designer, JavaScript, TypeScript, web development, web design, software engineer, full-stack developer"
                 author="Sen Doan"
-                url="https://sendoan.com/projects/landmark-pose-ai"
-                image="https://sendoan.com/images/landmark-pose-ai-preview.jpeg"
+                url="https://sendoan.com"
+                image="https://sendoan.com/images/portfolio.png"
             />
             <Hero />
             {/* <About />*/}

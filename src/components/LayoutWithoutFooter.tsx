213c5cacdb6daa5cb5d17ddab9cedd0ec5e6f49b
SenDoan <mariathanhsen@gmail.com>
2025-06-12 10:51:05 -0500

    add progress bar

--- src/components/LayoutWithoutFooter.tsx
+++ src/components/LayoutWithoutFooter.tsx
@@ -1,25 +1,35 @@
 import { Link, Outlet } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa6";
-import ThemeToggle from "./ThemeToggle";
+import { FaArrowLeft, FaRegCopyright } from "react-icons/fa6";
 
 export default function LayoutWithoutFooter() {
     return (
-        <div className="min-h-screen bg-gradient-to-br from-light-bg to-light-bg dark:from-dark-bg dark:to-dark-bg text-white font-mono">
+        <div className="min-h-screen bg-gradient-to-br from-light-bg to-light-bg text-white font-mono">
             <div className="min-h-full flex flex-col">
-                <nav className="bg-light-bg dark:bg-dark-bg p-4">
-                    <div className="flex justify-left items-left max-w-7xl mx-auto">
+                <nav className="bg-light-bg fixed top-0 left-0 right-0 z-50 shadow-md">
+                    <div className="flex justify-left items-left sm:px-0 px-4 py-4 max-w-7xl mx-auto">
                         {/* Centered name and icon */}
                         <div className="flex items-left justify-left flex-grow">
                             <Link to="/" className="flex items-left justify-left flex-grow">
-                                <FaArrowLeft className="size-6 text-dark-bg dark:text-light-bg" />
+                                <FaArrowLeft className="size-6 text-dark-bg" />
                             </Link>
                         </div>
-                        <div className="w-16 flex justify-end">
-                            <ThemeToggle />
-                        </div>
+
                     </div>
+                    <div className=" bg-dark-bg/80 my-progress-bar"></div>
                 </nav>
-                <Outlet />
+
+                <div className="mt-16 sm:mt-20">
+                    <Outlet />
+                </div>
+
+                <footer className="py-4 md:py-6 bg-light-bg text-dark-bg shadow-md mt-4">
+                    <div className="max-w-7xl mx-auto px-4 flex justify-center items-center gap-4 md:gap-0 text-center md:text-left">
+                        {/* Copyright Text */}
+                        <p className="text-xs md:text-sm flex items-center gap-1 text-dark-bg ">
+                            <FaRegCopyright className="inline-block size-3" /> {new Date().getFullYear()} Sen Doan
+                        </p>
+                    </div>
+                </footer>
             </div>
         </div>
     );
--- src/components/pages/LandmarkPoseAI.tsx
+++ src/components/pages/LandmarkPoseAI.tsx
@@ -7,8 +7,8 @@ import { FaCameraRetro, FaCheck, FaHandshake } from "react-icons/fa6";
 
 export default function LandmarkPoseAI() {
     return (
-        <section className="max-w-4xl mx-auto px-4 py-12 text-neutral-800 dark:text-neutral-100">
-            <h2 className="text-3xl font-bold mb-4"><FaCameraRetro className="inline-block text-neutral-800 dark:text-neutral-100" /> Landmark Pose AI</h2>
+        <section className="max-w-4xl mx-auto px-4 py-12 text-neutral-800 ">
+            <h2 className="text-3xl font-bold mb-4"><FaCameraRetro className="inline-block text-neutral-800 " /> Landmark Pose AI</h2>
 
             <p className="text-lg mb-6">
                 I discovered this project through <Link to="https://www.linkedin.com/posts/nhien-luu-434960173_ai-computervision-python-activity-7329281823711080449-0Kqj?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFFe0hwBZ1_0MGN0Z5gG_6qifhlPvt7dh1s" target="_blank" className="underline" >a LinkedIn post</Link> by Nhien and reached out to collaborate. I proposed the idea of training a model using HP AI Studio and contributed by building a responsive, user-friendly frontend with React and Tailwind CSS.
@@ -16,12 +16,12 @@ export default function LandmarkPoseAI() {
 
             <div className="space-y-6">
 
-                <div className="border-b border-dark-accent dark:border-neutral-100 pb-4">
+                <div className="border-b border-dark-accent  pb-4">
                     <Link
                         to="https://github.com/quangnhien/photo-posing-assistant"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-flex items-center gap-2 text-light-text dark:text-neutral-100 hover:text-light-accent dark:hover:text-dark-accent transform hover:rotate-6 transition-transform duration-200"
+                        className="inline-flex items-center gap-2 text-light-text  hover:text-light-accent  transform hover:rotate-6 transition-transform duration-200"
                     >
                         Visit Project <FaExternalLinkAlt className="inline" />
                     </Link>
@@ -82,7 +82,7 @@ export default function LandmarkPoseAI() {
                         ].map((tech) => (
                             <span
                                 key={tech}
-                                className="dark:bg-light-bg bg-dark-bg dark:text-light-text text-neutral-100 px-3 py-1 rounded text-sm"
+                                className=" bg-dark-bg text-neutral-100 px-3 py-1 rounded text-sm"
                             >
                                 {tech}
                             </span>
@@ -92,10 +92,10 @@ export default function LandmarkPoseAI() {
 
                 <div>
                     <h3 className="text-xl font-semibold mb-4"><FaHandshake className="inline-block text-yellow-500" /> Collaborator</h3>
-                    <div className="flex items-center gap-4 dark:bg-light-bg bg-dark-bg p-4 rounded-lg shadow-sm">
+                    <div className="flex items-center gap-4  bg-dark-bg p-4 rounded-lg shadow-sm">
                         <div className="gap-4 space-y-2">
-                            <p className="font-semibold text-lg dark:text-light-text text-neutral-100">Nhien Luu</p>
-                            <p className="text-sm dark:text-light-text text-neutral-100">
+                            <p className="font-semibold text-lg  text-neutral-100">Nhien Luu</p>
+                            <p className="text-sm  text-neutral-100">
                                 ML Engineer & Co-Creator of Landmark Pose AI
                             </p>
                             <div className="flex gap-3 md:gap-4">
@@ -103,7 +103,7 @@ export default function LandmarkPoseAI() {
                                     to="https://www.linkedin.com/in/nhien-luu-434960173/"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="dark:text-light-text text-neutral-100 hover:dark:text-light-accent hover:text-dark-accent transform hover:rotate-12 transition-transform duration-200"
+                                    className=" text-neutral-100  hover:text-dark-accent transform hover:rotate-12 transition-transform duration-200"
                                 >
                                     <FaLinkedinIn className="inline-block size-4 md:size-5" />
                                 </Link>
@@ -111,7 +111,7 @@ export default function LandmarkPoseAI() {
                                     to="https://github.com/quangnhien"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="dark:text-light-text text-neutral-100 hover:dark:text-light-accent hover:text-dark-accent transform hover:rotate-12 transition-transform duration-200"
+                                    className=" text-neutral-100  hover:text-dark-accent transform hover:rotate-12 transition-transform duration-200"
                                 >
                                     <FaGithub className="inline-block size-4 md:size-5" />
                                 </Link>
@@ -123,25 +123,25 @@ export default function LandmarkPoseAI() {
                 <div>
                     <div className="w-full flex flex-col justify-center items-center gap-6">
                         <div className="w-full mx-auto">
-                            <h6 className="text-xl font-semibold mb-4"><PiFilmSlateFill className="text-light-text dark:text-white inline-block" /> Search Feature</h6>
+                            <h6 className="text-xl font-semibold mb-4"><PiFilmSlateFill className="text-light-text  inline-block" /> Search Feature</h6>
                             <img
                                 src={searchGift}
                                 alt='Search Demo'
                                 className="w-full h-full object-contain rounded-lg mb-4 border-2 border-white bg-white p-2 transition-transform duration-200 transform"
                             />
                             <Link to="https://youtu.be/VibRBNcVG84?si=CJemrwA9PV9xrT7_"
-                                className="inline-flex items-center gap-2 text-light-text dark:text-neutral-100 hover:text-light-accent dark:hover:text-dark-accent transform hover:rotate-6 transition-transform duration-200">View on Youtube <FaExternalLinkAlt className="inline" /></Link>
+                                className="inline-flex items-center gap-2 text-light-text  hover:text-light-accent  transform hover:rotate-6 transition-transform duration-200">View on Youtube <FaExternalLinkAlt className="inline" /></Link>
                         </div>
 
                         <div className="w-full mx-auto">
-                            <h6 className="text-xl font-semibold mb-4"><PiFilmSlateFill className="text-light-text dark:text-white inline-block" /> Compare Feature</h6>
+                            <h6 className="text-xl font-semibold mb-4"><PiFilmSlateFill className="text-light-text inline-block" /> Compare Feature</h6>
                             <img
                                 src={compoareGift}
                                 alt='Compare Demo'
                                 className="w-full h-full object-contain rounded-lg mb-4 border-2 border-white bg-white p-2 transition-transform duration-200 transform"
                             />
                             <Link to="https://youtu.be/Gw2hniW6zFE?si=XXIzKuP_5MU6tAh1"
-                                className="inline-flex items-center gap-2 text-light-text dark:text-neutral-100 hover:text-light-accent dark:hover:text-dark-accent transform hover:rotate-6 transition-transform duration-200">View on Youtube <FaExternalLinkAlt className="inline" /></Link>
+                                className="inline-flex items-center gap-2 text-light-text  hover:text-light-accent  transform hover:rotate-6 transition-transform duration-200">View on Youtube <FaExternalLinkAlt className="inline" /></Link>
                         </div>
                     </div>
                 </div>

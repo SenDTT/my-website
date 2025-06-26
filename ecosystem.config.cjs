e1ac181804c9492c78cfdd60315d2054e913ee98
SenDoan <mariathanhsen@gmail.com>
2025-06-26 15:11:16 -0500

    update script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "SenDDT MW",
-      script: "vite serve",
+      script: "yarn serve",
       env: {
         PORT: 3008,
         HOST: "127.0.0.1",
